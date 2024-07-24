2419aa3d4936f5f47aef46acdb1f2280dbe1f126
gh jo <whrksp126@gmail.com>
2024-07-24 19:36:47 +0900

    save

--- App.js
+++ App.js
@@ -1,13 +1,41 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { StyleSheet, SafeAreaView, StatusBar, useColorScheme, BackHandler, Alert, ToastAndroid } from 'react-native';
 import { WebView } from 'react-native-webview';
+import * as WebBrowser from 'expo-web-browser';
+import * as AuthSession from 'expo-auth-session';
+
+WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
-  const colorScheme = useColorScheme(); // 시스템 테마 조회
+  const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === 'dark';
-
   const webViewRef = useRef(null);
-  const [lastBackPressed, setLastBackPressed] = useState(0); // 마지막 백 버튼 누른 시간
+  const [lastBackPressed, setLastBackPressed] = useState(0);
+
+  const redirectUri = AuthSession.makeRedirectUri({
+    useProxy: true,
+    scheme: 'HeyVoca'
+
+  });
+
+  const [request, response, promptAsync] = AuthSession.useAuthRequest(
+    {
+      clientId: '',
+      scopes: ['profile', 'email', 'openid', 'https://www.googleapis.com/auth/drive.file'],
+      redirectUri,
+    },
+    {
+      authorizationEndpoint: 'https://accounts.google.com/o/oauth2/v2/auth',
+    }
+  );
+
+  useEffect(() => {
+    if (response?.type === 'success') {
+      const { code } = response.params;
+      console.log('Auth code:', code);
+      webViewRef.current.injectJavaScript(`handleAuthSuccess('${code}')`);
+    }
+  }, [response]);
 
   useEffect(() => {
     const backAction = () => {
@@ -22,54 +50,59 @@ export default function App() {
       }
       return false;
     };
+
     const backHandler = BackHandler.addEventListener('hardwareBackPress', backAction);
     return () => backHandler.remove();
   }, [lastBackPressed]);
+
   const handleMessage = (event) => {
-    try {
-      const data = JSON.parse(event.nativeEvent.data);
-      if (data.type === 'alert') {
-        // alert 메시지를 네이티브 Alert로 처리하여 제목을 제거
-        Alert.alert('', data.message, [{ text: '확인' }], { cancelable: false });
-      } else if (data.type === 'isBackable') {
-        if (data.value) {
-          webViewRef.current.goBack();
-        } else {
-          handleExitApp();
+    const message = event.nativeEvent.data;
+    if (message === 'launchGoogleAuth') {
+      promptAsync();
+    } else {
+      try {
+        const data = JSON.parse(message);
+        if (data.type === 'alert') {
+          Alert.alert('', data.message, [{ text: 'OK' }], { cancelable: false });
+        } else if (data.type === 'isBackable') {
+          if (data.value) {
+            webViewRef.current.goBack();
+          } else {
+            handleExitApp();
+          }
         }
+      } catch (error) {
+        console.error('Error parsing message:', error);
       }
-    } catch (error) {
-      console.error('Error parsing message:', error);
     }
   };
 
   const handleExitApp = () => {
     const now = Date.now();
     if (lastBackPressed && (now - lastBackPressed) < 2000) {
-      BackHandler.exitApp(); // 2초 이내에 다시 호출되면 앱 종료
+      BackHandler.exitApp();
     } else {
-      ToastAndroid.show(`'뒤로' 버튼을 한 번 더 누르면 종료됩니다.`, ToastAndroid.SHORT);
-      setLastBackPressed(now); // 현재 시간을 저장
+      ToastAndroid.show('Press back again to exit.', ToastAndroid.SHORT);
+      setLastBackPressed(now);
     }
   };
 
   return (
     <SafeAreaView style={[styles.container, { backgroundColor: isDarkMode ? '#000' : '#fff' }]}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={isDarkMode ? '#000' : '#fff'}
-      />
+      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={isDarkMode ? '#000' : '#fff'} />
       <WebView
         source={{ uri: 'https://voca.ghmate.com' }}
         ref={webViewRef}
         javaScriptEnabled={true}
-        bounces={false}  // iOS에서 스크롤 바운스 비활성화
-        overScrollMode="never" // Android에서 스크롤 바운스 비활성화
+        bounces={false}
+        overScrollMode="never"
         onMessage={handleMessage}
         injectedJavaScript={`
-          // JavaScript에서 alert 함수를 오버라이드하여 네이티브 앱으로 메시지 전달
           window.alert = function(message) {
             window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'alert', message: message }));
+          };
+          document.querySelector('.google_btn').onclick = function() {
+            window.ReactNativeWebView.postMessage('launchGoogleAuth');
           };
         `}
       />
