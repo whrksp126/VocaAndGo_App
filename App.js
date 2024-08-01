f5400df5963c16af9e14c420b79476bad7eb4b2b
whrksp126 <whrksp126@gmail.com>
2024-08-02 02:31:13 +0900

    save

--- App.js
+++ App.js
@@ -7,50 +7,42 @@ import * as Linking from 'expo-linking';
 WebBrowser.maybeCompleteAuthSession();
 
 export default function App() {
+  const FRONT_URL = `https://voca.ghmate.com`
+  const BACK_URL = `https://vocaandgo.ghmate.com`
   const colorScheme = useColorScheme();
   const isDarkMode = colorScheme === 'dark';
   const webViewRef = useRef(null);
   const [lastBackPressed, setLastBackPressed] = useState(0);
 
   const handleAuth = async () => {
-    const authUrl = 'https://vocaandgo.ghmate.com/login/google';
+    const authUrl = `${BACK_URL}/login/google?device_type=app`;
     const result = await WebBrowser.openAuthSessionAsync(authUrl);
-
     if (result.type === 'success' && result.url) {
-      // 브라우저 팝업을 종료합니다.
-      WebBrowser.dismissAuthSession();
-      // URL에서 토큰을 추출합니다.
       const { token, email, name, status } = Linking.parse(result.url).queryParams;
-      // WebView로 인증 정보를 전달합니다.
       if (webViewRef.current) {
-        webViewRef.current.postMessage(JSON.stringify({
-          type: 'authSuccess',
-          token,
-          email,
-          name,
-          status
-        }));
+        if(status == 200){
+          const newUrl = `${FRONT_URL}/html/login.html?token=${token}&email=${email}&name=${name}&status=${status}`;
+          webViewRef.current.injectJavaScript(`window.location.href = '${newUrl}';`);
+        } else {
+          webViewRef.current.injectJavaScript(`alert('Login failed with status ${status}');`);
+        }
       }
     }
   };
 
   useEffect(() => {
     const handleUrl = (event) => {
-      console.log("Received URL from event: ", event.url); // 이벤트로 받은 URL 로그 출력
       const { token, email, name, status } = Linking.parse(event.url).queryParams;
       if (webViewRef.current) {
-        webViewRef.current.postMessage(JSON.stringify({
-          type: 'authSuccess',
-          token,
-          email,
-          name,
-          status
-        }));
+        if(status==200){
+          const newUrl = `${FRONT_URL}/html/login.html?token=${token}&email=${email}&name=${name}&status=${status}`;
+          webViewRef.current.injectJavaScript(`window.location.href = '${newUrl}';`);
+        }else {
+          webViewRef.current.injectJavaScript(`alert('Login failed with status ${status}');`);
+        }
       }
     };
-
-    Linking.addEventListener('url', handleUrl);
-
+    const linkingEvent = Linking.addEventListener('url', handleUrl);
     return () => {
       Linking.removeEventListener('url', handleUrl);
     };
@@ -110,21 +102,26 @@ export default function App() {
     <SafeAreaView style={[styles.container, { backgroundColor: isDarkMode ? '#000' : '#fff' }]}>
       <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} backgroundColor={isDarkMode ? '#000' : '#fff'} />
       <WebView
-        source={{ uri: 'https://voca.ghmate.com' }}
+        source={{ uri: `${FRONT_URL}` }}
         ref={webViewRef}
         javaScriptEnabled={true}
         bounces={false}
         overScrollMode="never"
         onMessage={handleMessage}
-        userAgent="HeyVocaWebView"
+        userAgent="HeyVoca"
         injectedJavaScript={`
           window.alert = function(message) {
             window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'alert', message: message }));
-          };
-          document.querySelector('.google_btn').onclick = function() {
-            window.ReactNativeWebView.postMessage('launchGoogleAuth');
-          };
+          };  
         `}
+        // injectedJavaScript={`
+        //   window.alert = function(message) {
+        //     window.ReactNativeWebView.postMessage(JSON.stringify({ type: 'alert', message: message }));
+        //   };
+        //   document.querySelector('.google_btn').onclick = function() {
+        //     window.ReactNativeWebView.postMessage('launchGoogleAuth');
+        //   };
+        // `}
       />
     </SafeAreaView>
   );
